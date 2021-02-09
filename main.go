f6c8de813b3e6a192a1a61783fb490503b499a6d
nwiizo <syu.m.5151@gmail.com>
2021-02-09 06:42:57 +0000

    関数にまとめる

--- main.go
+++ main.go
@@ -7,37 +7,22 @@ import (
 )
 
 func main() {
-	fmt.Println("docker ps -aq | xargs sudo docker stop")
 	// sudo docker ps -aq | xargs sudo docker stop
-	dockerstop := "sudo docker ps -aq | xargs sudo docker stop"
-	outstop, err := exec.Command("sh", "-c", dockerstop).Output()
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	fmt.Printf("output: %s", outstop)
+	Command_output("sudo docker ps -aq | xargs sudo docker stop")
 	// sudo docker ps -aq | xargs sudo docker rm
-	dockerrm := "sudo docker ps -aq | xargs sudo docker rm"
-	outrm, err := exec.Command("sh", "-c", dockerrm).Output()
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	fmt.Printf("output: %s", outrm)
+	Command_output("sudo docker ps -aq | xargs sudo docker rm")
 	// sudo docker network ls -q | xargs sudo docker network rm
-	dockernr := "sudo docker network ls -q | xargs sudo docker network rm"
-	outnr, err := exec.Command("sh", "-c", dockernr).Output()
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-	fmt.Printf("output: %s", outnr)
+	Command_output("sudo docker network ls -q | xargs sudo docker network rm")
 	// sudo service docker stop
-	dockerservicestop := "sudo service docker stop"
-	outservicestop, err := exec.Command("sh", "-c", dockerservicestop).Output()
+	Command_output("sudo service docker stop")
+}
+
+func Command_output(cmd string) {
+	fmt.Printf("cmd: %s \n", cmd)
+	output, err := exec.Command("sh", "-c", cmd).Output()
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-	fmt.Printf("output: %s", outservicestop)
+	fmt.Printf("output: %s \n", output)
 }
